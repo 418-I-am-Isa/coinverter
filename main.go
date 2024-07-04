1c36bcd66c29c67d94520919bccec27951883ef7
nirogu <nirogu@outlook.com>
2024-07-04 12:08:15 -0500

    Call conversion API from main

--- main.go
+++ main.go
@@ -1,37 +1,43 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"sort"
+	"strings"
+
 	tea "github.com/charmbracelet/bubbletea"
 )
 
- 
 type model struct {
-	cursor   int
-	choices  []string
-	selected map[int]struct{}
+	cursor       int
+	choices      []string
+	selected     map[int]struct{}
+	baseCurrency int
 }
 
-type Currency struct {
-    Symbol        string `json:"symbol"`
-    Name          string `json:"name"`
-    SymbolNative  string `json:"symbol_native"`
-    DecimalDigits int    `json:"decimal_digits"`
-    Rounding      int    `json:"rounding"`
-    Code          string `json:"code"`
-    NamePlural    string `json:"name_plural"`
+type CurrencyList struct {
+	Symbol        string `json:"symbol"`
+	Name          string `json:"name"`
+	SymbolNative  string `json:"symbol_native"`
+	DecimalDigits int    `json:"decimal_digits"`
+	Rounding      int    `json:"rounding"`
+	Code          string `json:"code"`
+	NamePlural    string `json:"name_plural"`
 }
 
-type Response struct {
-    Data map[string]Currency `json:"data"`
+type CurrencyListResponse struct {
+	Data map[string]CurrencyList `json:"data"`
 }
 
-func getCurrencies()([]string){
+type ConversionResponse struct {
+	Data map[string]float64 `json:"data"`
+}
+
+func getCurrencies() []string {
 	baseURL := "https://api.freecurrencyapi.com/v1/currencies"
 	apiKey := os.Getenv("API_KEY")
 	if len(apiKey) == 0 {
@@ -51,31 +57,71 @@ func getCurrencies()([]string){
 	data, err := io.ReadAll(resp.Body)
 	resp.Body.Close()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", url, err)
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 
-	var response Response
+	var response CurrencyListResponse
 	if err = json.Unmarshal(data, &response); err != nil {
 		fmt.Fprintf(os.Stderr, "Error unmarshalling %s: %v\n", url, err)
 		os.Exit(1)
 	}
 	choices := make([]string, 0, len(response.Data))
 	for key, currency := range response.Data {
-		choices = append(choices, key + " ("+ currency.Name + ")")
-    }
+		choices = append(choices, key+" ("+currency.Name+")")
+	}
 	sort.Strings(choices)
 	return choices
 }
 
+func conversion(baseCurrency string, targetCurrencies []string) {
+	baseURL := "https://api.freecurrencyapi.com/v1/latest"
+	apiKey := os.Getenv("API_KEY")
+	if len(apiKey) == 0 {
+		fmt.Fprintln(os.Stderr, "Error. No API key found in environment valiables. Set it in the API_KEY variable.")
+		os.Exit(1)
+	}
+
+	apiKeyParam := "apikey=" + apiKey
+	baseCurrencyParam := "base_currency=" + baseCurrency
+	currenciesParam := "currencies=" + strings.Join(targetCurrencies, ",")
+	url := baseURL + "?" + strings.Join([]string{apiKeyParam, baseCurrencyParam, currenciesParam}, "&")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+	if status := resp.StatusCode; status != 200 {
+		fmt.Fprintf(os.Stderr, "HTTP response code: %d\n", status)
+		os.Exit(1)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, currency := range targetCurrencies {
+		var response ConversionResponse
+		if err = json.Unmarshal(data, &response); err != nil {
+			fmt.Fprintf(os.Stderr, "Error unmarshalling %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		value := response.Data[currency]
+
+		fmt.Printf("1 %s = %f %s\n", baseCurrency, value, currency)
+	}
+}
+
 func initialModel() model {
 	return model{
-		choices: getCurrencies(),
-
-		// A map which indicates which choices are selected. We're using
-		// the map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected: make(map[int]struct{}),
+		choices:      getCurrencies(),
+		baseCurrency: -1,
+		selected:     make(map[int]struct{}),
 	}
 }
 
@@ -97,13 +143,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if m.cursor < len(m.choices)-1 {
 				m.cursor++
 			}
-		case "enter", " ":
+		case " ":
 			_, ok := m.selected[m.cursor]
 			if ok {
 				delete(m.selected, m.cursor)
 			} else {
 				m.selected[m.cursor] = struct{}{}
 			}
+		case "enter":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			}
+			m.baseCurrency = m.cursor
 		}
 	}
 
@@ -120,7 +172,9 @@ func (m model) View() string {
 		}
 
 		checked := " "
-		if _, ok := m.selected[i]; ok {
+		if m.baseCurrency == i {
+			checked = "•"
+		} else if _, ok := m.selected[i]; ok {
 			checked = "x"
 		}
 
@@ -134,9 +188,21 @@ func (m model) View() string {
 
 func main() {
 	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
+	m, err := p.Run()
+	if err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
+	if m, ok := m.(model); ok && m.baseCurrency != -1 {
+		baseCurrency := m.choices[m.baseCurrency]
+		baseCurrency = strings.Split(baseCurrency, " ")[0]
+		targetCurrencies := make([]string, 0, len(m.selected))
+		for idx := range m.selected {
+			target := m.choices[idx]
+			target = strings.Split(target, " ")[0]
+			targetCurrencies = append(targetCurrencies, target)
+		}
+		sort.Strings(targetCurrencies)
+		conversion(baseCurrency, targetCurrencies)
+	}
 }
-
